3c8648182883915657ea6d88f0b7d06b24c7524b
a25patel <aanaletap2510@gmail.com>
2015-07-23 13:21:53 -0600

    show

--- routes/articles.js
+++ routes/articles.js
@@ -10,9 +10,23 @@ router.get('/articles', function(req,res,next){
   });
 });
 
+// GET my new article page!
 router.get('/articles/new', function(req, res, next) {
   res.render('articles/new');
 });
+
+//POST information into my database
+router.post('/articles', function(req,res,next){
+  articleCollection.insert({
+    title: req.body.title,
+    url: req.body.url,
+    background: req.body.background,
+    excerpt: req.body.excerpt,
+    body: req.body.articleBody,
+  });
+  res.redirect('/articles');
+});
+
 
 
 router.get('/articles/edit', function(req, res, next) {
